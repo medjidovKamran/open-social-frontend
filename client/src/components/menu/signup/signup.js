80f78301cf03b60fb3043d94dfa2b986a6a46b10
Taras Makarchuk <2278452@gmail.com>
2020-04-18 16:45:52 +0300

    validation added

--- client/src/components/menu/signup/signup.js
+++ client/src/components/menu/signup/signup.js
@@ -2,73 +2,157 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
-import s from './signup.module.scss';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import s from './signup.module.scss';
+import errorStyles from '../../validation/validation.module.scss';
+
+const initialValues = {
+  birthdayDate: '',
+  confirmPassword: '',
+  email: '',
+  firstName: '',
+  lastName: '',
+  password: '',
+};
+
+const SCHEMA_VALUES = {
+  MAX_NAME_LENGTH: 50,
+  MAX_PASSWORD_LENGTH: 30,
+  MIN_NAME_LENGTH: 2,
+  MIN_PASSWORD_LENGTH: 6,
+  REG_EXP_NAME: /^[/a-zA-Zа-яА-Я\-a-zA-Zа-яА-Я]+$/,
+};
+
+const validationSchema = Yup.object({
+  birthdayDate: Yup.date().required('* Birthday date is required'),
+  confirmPassword: Yup.string()
+    .min(SCHEMA_VALUES.MIN_PASSWORD_LENGTH, '* Should be 6 characters or more')
+    .max(SCHEMA_VALUES.MAX_PASSWORD_LENGTH, '* Should be 30 characters or less')
+    .required('* Confirm password is required')
+    .oneOf([Yup.ref('password'), null], '* Passwords should match'),
+  email: Yup.string()
+    .email('* Invalid email address')
+    .required('* Email is required'),
+  firstName: Yup.string()
+    .matches(
+      SCHEMA_VALUES.REG_EXP_NAME,
+      '* First name should contain only letters',
+    )
+    .min(SCHEMA_VALUES.MIN_NAME_LENGTH, '* Should be 2 characters or more')
+    .max(SCHEMA_VALUES.MAX_NAME_LENGTH, '* Should be 50 characters or less')
+    .required('* First name is required'),
+  lastName: Yup.string()
+    .matches(
+      SCHEMA_VALUES.REG_EXP_NAME,
+      '* Last name should contain only letters',
+    )
+    .min(SCHEMA_VALUES.MIN_NAME_LENGTH, '* Should be 2 characters or more')
+    .max(SCHEMA_VALUES.MAX_NAME_LENGTH, '* Should be 50 characters or less')
+    .required('* Last name is required'),
+  password: Yup.string()
+    .min(SCHEMA_VALUES.MIN_PASSWORD_LENGTH, '* Should be 6 characters or more')
+    .max(SCHEMA_VALUES.MAX_PASSWORD_LENGTH, '* Should be 30 characters or less')
+    .required('* Password is required'),
+});
 
 const Signup = () => {
-  const [registerForm, setRegisterForm] = useState({});
   const [birthdayDate, setDate] = useState(new Date());
+  const formik = useFormik({
+    initialValues,
+    onSubmit: values => {
+      if (values.password === values.confirmPassword) {
+        console.log(JSON.stringify(values, null, 2));
+      }
+    },
+    validationSchema,
+  });
 
-  const handleInput = event => {
-    setRegisterForm({
-      ...registerForm,
-      [event.target.name]: event.target.value,
-    });
+  const getBirthdayDate = () => {
+    formik.values.birthdayDate = birthdayDate;
+    return formik.values.birthdayDate;
   };
-
-  const handleDatePickerChange = birthdayDate => setDate(birthdayDate);
-
-  const getUserData = () => {
-    return { ...registerForm, birthdayDate };
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    if (registerForm.password !== registerForm.confirmPassword)
-      alert('Your password and confirmation password do not match.');
-    // console.log(registerForm);
-    // todo use userData to send to server
-    console.log(getUserData());
+  const setBirthDate = birthdayDate => {
+    setDate(birthdayDate);
   };
 
   return (
     <main className={s.signupMain}>
-      <form className={s.signupForm} onSubmit={handleSubmit}>
+      <form className={s.signupForm} onSubmit={formik.handleSubmit}>
         <h1>Register</h1>
         <p>* all fields are required</p>
         <label htmlFor="first-name">
           First name:
           <input
+            className={
+              formik.errors.firstName &&
+              formik.touched.firstName &&
+              errorStyles.ErrorInput
+            }
             type="text"
             id="first-name"
             name="firstName"
-            required
-            onChange={handleInput}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.firstName}
           />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <div className={errorStyles.ErrorMessage}>
+              {formik.errors.firstName}
+            </div>
+          ) : null}
         </label>
         <label htmlFor="last-name">
           Last name:
           <input
+            className={
+              formik.errors.lastName &&
+              formik.touched.lastName &&
+              errorStyles.ErrorInput
+            }
             type="text"
             id="last-name"
             name="lastName"
-            required
-            onChange={handleInput}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.lastName}
           />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <div className={errorStyles.ErrorMessage}>
+              {formik.errors.lastName}
+            </div>
+          ) : null}
         </label>
         <label htmlFor="email">
           Email:
           <input
+            className={
+              formik.errors.email &&
+              formik.touched.email &&
+              errorStyles.ErrorInput
+            }
             type="email"
             id="email"
             name="email"
-            required
-            onChange={handleInput}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <div className={errorStyles.ErrorMessage}>
+              {formik.errors.email}
+            </div>
+          ) : null}
         </label>
         <label htmlFor="birthday">
           Birthday:
           <DatePicker
+            className={
+              formik.errors.birthdayDate &&
+              formik.touched.birthdayDate &&
+              errorStyles.ErrorInput
+            }
             type="date"
             name="birthdayDate"
             selected={birthdayDate}
@@ -78,31 +162,59 @@ const Signup = () => {
             showYearDropdown
             dropdownMode="select"
             maxDate={new Date()}
-            required
-            onChange={handleDatePickerChange}
+            onChange={setBirthDate}
+            onBlur={formik.handleBlur}
+            value={getBirthdayDate()}
           />
+          {formik.touched.birthdayDate && formik.errors.birthdayDate ? (
+            <div className={errorStyles.ErrorMessage}>
+              {formik.errors.birthdayDate}
+            </div>
+          ) : null}
         </label>
         <label htmlFor="password">
           Password:
           <input
+            className={
+              formik.errors.password &&
+              formik.touched.password &&
+              errorStyles.ErrorInput
+            }
             type="password"
             id="password"
             name="password"
-            required
-            onChange={handleInput}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
           />
+          {formik.touched.password && formik.errors.password ? (
+            <div className={errorStyles.ErrorMessage}>
+              {formik.errors.password}
+            </div>
+          ) : null}
         </label>
         <label htmlFor="confirm-password">
           Confirm:
           <input
+            className={
+              formik.errors.confirmPassword &&
+              formik.touched.confirmPassword &&
+              errorStyles.ErrorInput
+            }
             type="password"
             id="confirm-password"
             name="confirmPassword"
-            required
-            onChange={handleInput}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.confirmPassword}
           />
+          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+            <div className={errorStyles.ErrorMessage}>
+              {formik.errors.confirmPassword}
+            </div>
+          ) : null}
         </label>
-        <button className={s.SignupButton} type="button" onClick={handleSubmit}>
+        <button className={s.SignupButton} type="submit">
           Signup
         </button>
         <p>
