2742f5a056148bee6aadda4e2b3525c1ba02a103
Maxe1 <hatamaxel@gmail.com>
2020-05-10 13:08:52 +0300

    User profile

--- client/src/components/Chats/index.js
+++ client/src/components/Chats/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Chats from './Chats';
 import Layout from '../Layout';
 import setCurrentTab from '../../actions/menu';
-import { getUsersData } from '../../actions/chats';
+import { getUserData, getUsersData } from '../../actions/chats';
+
+import apiClient from '../../utils/axios-with-auth';
 
 export default async function action({ store: { dispatch } }) {
   dispatch(setCurrentTab('Chats'));
   dispatch(getUsersData());
+  dispatch(getUserData(apiClient.userId()));
 
   await dispatch;
 
