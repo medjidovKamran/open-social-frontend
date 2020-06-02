c2d68a45045d37468a71026b91c887292f49383a
unknown <you@example.com>
2020-06-02 10:54:10 +0300

    fixed css usersPage

--- client/src/components/UsersPage/User.js
+++ client/src/components/UsersPage/User.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/withStyles';
-import s from './UsersPage.scss';
-import Link from "../Link";
-import Button from "@material-ui/core/Button";
-import avatar from '../../assets/avatar1.png'
+import s from './User.scss';
+import Link from '../Link';
+import Button from '@material-ui/core/Button';
+import avatar from '../../assets/avatar1.png';
 
-let User = ({user}) => {
-  return (
-    <div>
-        <span>
-            <div>
-                <Link to={'/' + user.id}>
-                    <img
-                      className={s.UsersAvatar}
-                      src={avatar}
-                    />
-                </Link>
-            </div>
-            <div>
-              <Button variant="contained" color="primary">Follow</Button>
-            </div>
-        </span>
-      <span>
-            <div>{user.firstName} {user.lastName}</div><div>{user.status}</div>
-        </span>
-    </div>
-  )
-}
-
+const User = ({ user }) => {
+	const { firstName, lastName, status } = user;
+	return (
+		<div className={s.userConatainer}>
+			<span>
+				<div>
+					<Link to={'/' + user.id}>
+						<img className={s.UsersAvatar} src={avatar} />
+					</Link>
+				</div>
+				<div>
+					{firstName} {lastName}
+				</div>
+				<div>{status}</div>
+				<div>
+					<Button variant="contained" color="primary">
+						Follow
+					</Button>
+				</div>
+			</span>
+		</div>
+	);
+};
 
 User.whyDidYouRender = true;
 export default withStyles(s)(React.memo(User));
--- client/src/components/UsersPage/UsersPage.js
+++ client/src/components/UsersPage/UsersPage.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/withStyles';
 import s from './UsersPage.scss';
-import {connect} from "react-redux";
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
-import {getUsersData} from "../../actions/users";
-import User from "./User";
+import { connect } from 'react-redux';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import { getUsersData } from '../../actions/users';
+import User from './User';
+import Loader from '../Loader/Loader';
 
+const UsersPage = ({ data, events, error, isLoading }) => {
+	return (
+		<div className={s.container}>
+			{/*	{isLoading && <Loader />}*/}
 
-const UsersPage = ({data}) => {
-  return (
-    <div className={s.form}>
-      <h3 className={s.heading}>Users</h3>
-      {data.map(u =>
-        <User
-          key={u.id}
-          user={u}
-        />
-      )
-      }
-    </div>
-  );
+			<h3 className={s.heading}>Users</h3>
+			{data.map((u) => <User key={u.id} user={u} />)}
+		</div>
+	);
 };
 
 UsersPage.whyDidYouRender = true;
 export default connect(
-  ({ users: { data, events, error, isLoading }}) => ({
-    data, events, error, isLoading
-  }),
-  {getUsersData},
+	({ users: { data, events, error, isLoading } }) => ({
+		data,
+		events,
+		error,
+		isLoading
+	}),
+	{ getUsersData }
 )(withStyles(bootstrap, s)(React.memo(UsersPage)));
