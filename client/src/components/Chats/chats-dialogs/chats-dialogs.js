d4ce41eb7cb6d0bcd4a6e8838cc58bebac85ff7d
Maxe1 <hatamaxel@gmail.com>
2020-06-10 21:53:22 +0300

    Fix dialog chats

--- client/src/components/Chats/chats-dialogs/chats-dialogs.js
+++ client/src/components/Chats/chats-dialogs/chats-dialogs.js
@@ -1,74 +1,79 @@
 import React, { useState } from 'react';
 import withStyles from 'isomorphic-style-loader/withStyles';
 import style from './chats-dialogs.module.scss';
-import avatar from '../../../assets/avatar.png'
+import avatar from '../../../assets/avatar.png';
 import Messages from './messages';
-import "emoji-mart/css/emoji-mart.css";
-import { Picker } from "emoji-mart";
+import emojii from 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
 
 const ChatsDialogs = () => {
-  const [emojiPickerState, SetEmojiPicker] = useState(false);
-  const [message, SetMessage] = useState("");
+	const [ emojiPickerState, SetEmojiPicker ] = useState(false);
+	const [ message, SetMessage ] = useState('');
 
-  let emojiPicker;
-  if (emojiPickerState) {
-    emojiPicker = (
-      <Picker
-        title="Pick your emoji…"
-        emoji="point_up"
-        onSelect={emoji => SetMessage(message + emoji.native)}
-      />
-    );
-  }
+	let emojiPicker;
+	if (emojiPickerState) {
+		emojiPicker = (
+			<Picker
+				title="Pick your emoji…"
+				emoji="point_up"
+				onSelect={(emoji) => SetMessage(message + emoji.native)}
+			/>
+		);
+	}
 
-  function triggerPicker(event) {
-    event.preventDefault();
-    SetEmojiPicker(!emojiPickerState);
-  }
+	function triggerPicker(event) {
+		event.preventDefault();
+		SetEmojiPicker(!emojiPickerState);
+	}
 
+	return (
+		<div className={style.wrapper}>
+			<header>
+				<div className={style.user}>
+					<a href="">
+						<img src={avatar} alt="not found" />
+					</a>
+					<div className={style.userName}>
+						<a href="" className={style.name}>
+							<span>Nika Jerrardo</span>
+						</a>
+						<span className={style.lastTime}>last online 5 hours ago</span>
+					</div>
+				</div>
+				<div className={style.buttons}>
+					<button className={style.attach} />
+					<button className={style.more} />
+				</div>
+			</header>
 
-  return (
-    <div className={style.wrapper}>
-      <header>
-        <div className={style.user}>
-          <a href=""><img src={avatar} alt="not found"></img></a>
-          <div className={style.userName}>
-            <a href="" className={style.name}><span>Nika Jerrardo</span></a>
-            <span className={style.lastTime}>last online 5 hours ago</span>
-          </div>
-        </div>
-        <div className={style.buttons}>
-          <button className={style.attach}></button>
-          <button className={style.more}></button>
-        </div>
-      </header>
-  
-      <Messages />
+			<Messages />
 
-      <div className={style.formLine}></div>
-      <div className={style.formWrapper}>
-        <button className={style.dropUpButton}></button>
-        <form action="">
-          <textarea name="" 
-                    id="" rows="1" 
-                    placeholder="Type a message here" 
-                    value={message} 
-                    onChange={event => SetMessage(event.target.value)}>
-          </textarea>
-          {emojiPicker}
-          <button className={style.smileButton} onClick={triggerPicker}></button>
-          <button type="submit" className={style.sendButton}></button>
-        </form>
-  
-        <div className="dropUp">
-          <img src="./img/Icon File.png" alt=""></img>
-          <img src="./img/Icon Photo.png" alt=""></img>
-          <img src="./img/Icon Video.png" alt=""></img>
-        </div>
-      </div>
-    </div>
-  )
+			<div className={style.formLine} />
+			<div className={style.formWrapper}>
+				<button className={style.dropUpButton} />
+				<form action="">
+					<textarea
+						name=""
+						id=""
+						rows="1"
+						placeholder="Type a message here"
+						value={message}
+						onChange={(event) => SetMessage(event.target.value)}
+					/>
+					{emojiPicker}
+					<button className={style.smileButton} onClick={triggerPicker} />
+					<button type="submit" className={style.sendButton} />
+				</form>
+
+				<div className="dropUp">
+					<img src="./img/Icon File.png" alt="" />
+					<img src="./img/Icon Photo.png" alt="" />
+					<img src="./img/Icon Video.png" alt="" />
+				</div>
+			</div>
+		</div>
+	);
 };
 // ChatsDialogs.whyDidYouRender = true;
-export default withStyles(style)(React.memo(ChatsDialogs));
+export default withStyles(style, emojii)(React.memo(ChatsDialogs));
 // export default ChatsDialogs;
