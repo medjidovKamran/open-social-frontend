b45e916b854aafb7021a857955114819516c2399
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-07-10 15:12:59 +0300

    redux

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,31 +1,42 @@
 import apiClient from '../utils/axios-with-auth';
 import {
-	apiURL,
-	USERS_DATA_LOADING,
-	USERS_DATA_LOADING_DATA_FAILURE,
-	USERS_DATA_LOADING_DATA_SUCCESS
+  apiURL,
+  USERS_DATA_LOADING,
+  USERS_DATA_LOADING_DATA_FAILURE,
+  USERS_DATA_LOADING_DATA_SUCCESS,
 } from '../constants';
 
-const userDataSuccess = (payload) => ({
-	payload,
-	type: USERS_DATA_LOADING_DATA_SUCCESS
+const userDataSuccess = payload => ({
+  payload,
+  type: USERS_DATA_LOADING_DATA_SUCCESS,
 });
 
-const userDataFailure = (error) => ({
-	error,
-	type: USERS_DATA_LOADING_DATA_FAILURE
+const userDataFailure = error => ({
+  error,
+  type: USERS_DATA_LOADING_DATA_FAILURE,
 });
 
 const userDataLoading = () => ({
-	type: USERS_DATA_LOADING
+  type: USERS_DATA_LOADING,
 });
 
-export const getUsersData = () => async (dispatch) => {
-	dispatch(userDataLoading());
-	try {
-		const { data } = await apiClient.get(`${apiURL}/api/v1/users`, `${apiURL}/api/v1/users?query=userName`);
-		dispatch(userDataSuccess(data));
-	} catch (error) {
-		dispatch(userDataFailure(error.message));
-	}
+export const getUsersData = () => async dispatch => {
+  dispatch(userDataLoading());
+  try {
+    const { data } = await apiClient.get(`${apiURL}/api/v1/users`);
+    dispatch(userDataSuccess(data));
+  } catch (error) {
+    dispatch(userDataFailure(error.message));
+  }
+};
+export const getUsersWithParams = ({ query }) => async dispatch => {
+  dispatch(userDataLoading({ query }));
+  try {
+    const { data } = await apiClient.get('${apiURL}/api/v1/users', {
+      search: query,
+    });
+    dispatch(userDataSuccess({ data }));
+  } catch (error) {
+    dispatch(userDataFailure(error.message));
+  }
 };
--- client/src/components/UsersPage/UserSearchPanel/UserSearchPanel.js
+++ client/src/components/UsersPage/UserSearchPanel/UserSearchPanel.js
@@ -1,70 +1,72 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getUsersData } from '../../../actions/users';
+import { getUsersWithParams } from '../../../actions/users';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/withStyles';
 import s from './UsersSearchPanel.scss';
 import SearchIcon from '@material-ui/icons/Search';
 
 class UserSearchPanel extends Component {
-	static propTypes = {
-		getUsersData: PropTypes.func.isRequired
-	};
-	state = {
-		query: ''
-	};
+  static propTypes = {
+    getUsersWithParams: PropTypes.func.isRequired,
+  };
+  state = {
+    query: '',
+  };
 
-	handleOnInputChange = (event) => {
-		const query = event.target.value;
-		if (!query) {
-			this.setState({ query });
-		} else {
-			this.setState({ query: event.target.value });
-		}
-	};
-	fetchData = () => {
-		this.props.getUsersData(this.state.query);
-	};
+  handleOnInputChange = event => {
+    const query = event.target.value;
+    if (!query) {
+      this.setState({ query });
+    } else {
+      this.setState({ query: event.target.value });
+    }
+  };
+  fetchData = ({ query }) => {
+    this.props.getUsersWithParams({
+      search: query,
+    });
+  };
 
-	render() {
-		const results = this.props.data.map((result) => {
-			return (
-				<a key={result.id} href={result.previewURL}>
-					<h6>{result.user}</h6>
-					<div>
-						<img src={result.previewURL} alt={result.user} />
-					</div>
-				</a>
-			);
-		});
-		return (
-			<div className={s.searchPanel}>
-				<label className={s.searchPanelDiv}>
-					<input
-						className={s.searchPanelInput}
-						type="text"
-						value={this.state.query}
-						placeholder="Search..."
-						onChange={this.handleOnInputChange}
-					/>
-					<button className={s.searchPanelIcon} onClick={this.fetchData}>
-						<SearchIcon style={{ color: 'white' }} />
-					</button>
-				</label>
-				{results}
-			</div>
-		);
-	}
+  render() {
+    const results = this.props.data.map(result => {
+      return (
+        <a key={result.id} href={result.previewURL}>
+          <h6>{result.user}</h6>
+          <div>
+            <img src={result.previewURL} alt={result.user} />
+          </div>
+        </a>
+      );
+    });
+    return (
+      <div className={s.searchPanel}>
+        <label className={s.searchPanelDiv}>
+          <input
+            className={s.searchPanelInput}
+            type="text"
+            value={this.state.query}
+            placeholder="Search..."
+            onChange={this.handleOnInputChange}
+          />
+          <button className={s.searchPanelIcon} onClick={this.fetchData}>
+            <SearchIcon style={{ color: 'white' }} />
+          </button>
+        </label>
+        {results}
+      </div>
+    );
+  }
 }
 
 UserSearchPanel.whyDidYouRender = true;
 export default connect(
-	({ users: { data, error, isLoading } }) => ({
-		data,
-		error,
-		isLoading
-	}),
-	{ getUsersData }
+  ({ users: { data, error, isLoading } }) => ({
+    data,
+    error,
+    isLoading,
+  }),
+  { getUsersWithParams },
 )(withStyles(s)(React.memo(UserSearchPanel)));
 
 // const resultsArray = [];
