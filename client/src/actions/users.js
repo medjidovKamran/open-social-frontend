dc704ea4830f97bb6fe1f04eb777cf9acd54ebaf
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-07-17 20:35:26 +0300

    redux

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,43 +1,42 @@
 import apiClient from '../utils/axios-with-auth';
 import {
-	apiURL,
-	USERS_DATA_LOADING,
-	USERS_DATA_LOADING_DATA_FAILURE,
-	USERS_DATA_LOADING_DATA_SUCCESS
+  apiURL,
+  USERS_DATA_LOADING,
+  USERS_DATA_LOADING_DATA_FAILURE,
+  USERS_DATA_LOADING_DATA_SUCCESS,
 } from '../constants';
 
-const userDataSuccess = (payload) => ({
-	payload,
-	type: USERS_DATA_LOADING_DATA_SUCCESS
+const userDataSuccess = payload => ({
+  payload,
+  type: USERS_DATA_LOADING_DATA_SUCCESS,
 });
 
-const userDataFailure = (error) => ({
-	error,
-	type: USERS_DATA_LOADING_DATA_FAILURE
+const userDataFailure = error => ({
+  error,
+  type: USERS_DATA_LOADING_DATA_FAILURE,
 });
 
 const userDataLoading = () => ({
-	type: USERS_DATA_LOADING
+  type: USERS_DATA_LOADING,
 });
 
-export const getUsersData = () => async (dispatch) => {
-	dispatch(userDataLoading());
-	try {
-		const { data } = await apiClient.get(`${apiURL}/api/v1/users`);
-		dispatch(userDataSuccess(data));
-	} catch (error) {
-		dispatch(userDataFailure(error.message));
-	}
+export const getUsersData = () => async dispatch => {
+  dispatch(userDataLoading());
+  try {
+    const { data } = await apiClient.get(`${apiURL}/api/v1/users`);
+    dispatch(userDataSuccess(data));
+  } catch (error) {
+    dispatch(userDataFailure(error.message));
+  }
 };
-export const getUsersWithParams = ({ query }) => async (dispatch) => {
-	dispatch(userDataLoading({ query }));
-	try {
-		const data = await apiClient.get(`${apiURL}/api/v1/users=%${search}%`, {
-			search: query
-		});
-		console.log(data);
-		dispatch(userDataSuccess(data));
-	} catch (error) {
-		dispatch(userDataFailure(error.message));
-	}
+export const getUsersWithParams = ({ search }) => async dispatch => {
+  dispatch(userDataLoading({ search }));
+  try {
+    console.log(search);
+    const data = await apiClient.get(`${apiURL}/api/v1/users=%${search}%`);
+    console.log(data);
+    dispatch(userDataSuccess(data));
+  } catch (error) {
+    dispatch(userDataFailure(error.message));
+  }
 };
