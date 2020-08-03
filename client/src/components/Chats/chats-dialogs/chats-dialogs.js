1494fad8701d834656c64133976e759c4b3f743d
Taras Makarchuk <2278452@gmail.com>
2020-08-03 16:23:10 +0300

    merge

--- client/src/components/Chats/chats-dialogs/chats-dialogs.js
+++ client/src/components/Chats/chats-dialogs/chats-dialogs.js
@@ -10,63 +10,63 @@ import iconPhoto from '../../../assets/chat/icon-photo.png';
 import iconVideo from '../../../assets/chat/icon-video.png';
 
 const ChatsDialogs = () => {
-	const [ emojiPickerState, SetEmojiPicker ] = useState(false);
-	const [ message, SetMessage ] = useState('');
+  const [emojiPickerState, SetEmojiPicker] = useState(false);
+  const [message, SetMessage] = useState('');
 
-	let emojiPicker;
-	if (emojiPickerState) {
-		emojiPicker = (
-			<Picker
-				title="Pick your emoji…"
-				emoji="point_up"
-				onSelect={(emoji) => SetMessage(message + emoji.native)}
-			/>
-		);
-	}
+  let emojiPicker;
+  if (emojiPickerState) {
+    emojiPicker = (
+      <Picker
+        title="Pick your emoji…"
+        emoji="point_up"
+        onSelect={emoji => SetMessage(message + emoji.native)}
+      />
+    );
+  }
 
-	function triggerPicker(event) {
-		event.preventDefault();
-		SetEmojiPicker(!emojiPickerState);
-	}
+  function triggerPicker(event) {
+    event.preventDefault();
+    SetEmojiPicker(!emojiPickerState);
+  }
 
-	return (
-		<div className={style.wrapper}>
-			<header>
-				<div className={style.user}>
-					<a href="">
-						<img src={avatar} alt="not found" />
-					</a>
-					<div className={style.userName}>
-						<a href="" className={style.name}>
-							<span>Nika Jerrardo</span>
-						</a>
-						<span className={style.lastTime}>last online 5 hours ago</span>
-					</div>
-				</div>
-				<div className={style.buttons}>
-					<button className={style.attach} />
-					<button className={style.more} />
-				</div>
-			</header>
+  return (
+    <div className={style.wrapper}>
+      <header>
+        <div className={style.user}>
+          <a href="">
+            <img src={avatar} alt="not found" />
+          </a>
+          <div className={style.userName}>
+            <a href="" className={style.name}>
+              <span>Nika Jerrardo</span>
+            </a>
+            <span className={style.lastTime}>last online 5 hours ago</span>
+          </div>
+        </div>
+        <div className={style.buttons}>
+          <button className={style.attach} />
+          <button className={style.more} />
+        </div>
+      </header>
 
-			<Messages />
+      <Messages />
 
-			<div className={style.formLine} />
-			<div className={style.formWrapper}>
-				<button className={style.dropUpButton} />
-				<form action="">
-					<textarea
-						name=""
-						id=""
-						rows="1"
-						placeholder="Type a message here"
-						value={message}
-						onChange={(event) => SetMessage(event.target.value)}
-					/>
-					{emojiPicker}
-					<button className={style.smileButton} onClick={triggerPicker} />
-					<button type="submit" className={style.sendButton} />
-				</form>
+      <div className={style.formLine} />
+      <div className={style.formWrapper}>
+        <button className={style.dropUpButton} />
+        <form action="">
+          <textarea
+            name=""
+            id=""
+            rows="1"
+            placeholder="Type a message here"
+            value={message}
+            onChange={event => SetMessage(event.target.value)}
+          />
+          {emojiPicker}
+          <button className={style.smileButton} onClick={triggerPicker} />
+          <button type="submit" className={style.sendButton} />
+        </form>
 
         <div className="dropUp">
           <img src={iconFile} alt="" />
