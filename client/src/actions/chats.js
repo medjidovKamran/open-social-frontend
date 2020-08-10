b2b82c9450020534911d5de496e294572c97dabb
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-08-10 07:47:47 +0300

    smth

--- client/src/actions/chats.js
+++ client/src/actions/chats.js
@@ -1,78 +1,73 @@
 import apiClient from '../utils/axios-with-auth';
 import {
-  USERS_CHAT_DATA_LOADING,
-  USERS_CHAT_DATA_LOADING_DATA_SUCCESS,
-  USERS_CHAT_DATA_LOADING_DATA_FAILURE,
-  USERS_CHAT_ADD_OWN_CHAT,
-  USERS_CHAT_RESET_STATE,
-  apiURL,
+	USERS_CHAT_DATA_LOADING,
+	USERS_CHAT_DATA_LOADING_DATA_SUCCESS,
+	USERS_CHAT_DATA_LOADING_DATA_FAILURE,
+	USERS_CHAT_ADD_OWN_CHAT,
+	USERS_CHAT_RESET_STATE,
+	apiURL
 } from '../constants';
 
-const userChatDataSuccess = payload => ({
-  payload,
-  type: USERS_CHAT_DATA_LOADING_DATA_SUCCESS,
+const userChatDataSuccess = (payload) => ({
+	payload,
+	type: USERS_CHAT_DATA_LOADING_DATA_SUCCESS
 });
 
-const newChatDataSuccess = payload => ({
-  payload,
-  type: USERS_CHAT_ADD_OWN_CHAT,
+const newChatDataSuccess = (payload) => ({
+	payload,
+	type: USERS_CHAT_ADD_OWN_CHAT
 });
 
-const userChatDataFailure = error => ({
-  error,
-  type: USERS_CHAT_DATA_LOADING_DATA_FAILURE,
+const userChatDataFailure = (error) => ({
+	error,
+	type: USERS_CHAT_DATA_LOADING_DATA_FAILURE
 });
 
 const userChatDataLoading = () => ({
-  type: USERS_CHAT_DATA_LOADING,
+	type: USERS_CHAT_DATA_LOADING
 });
 
 const clearChatState = () => ({
-  type: USERS_CHAT_RESET_STATE,
+	type: USERS_CHAT_RESET_STATE
 });
 
 // eslint-disable-next-line consistent-return
-export const getUsersChatData = ({ limit, offset }) => async dispatch => {
-  dispatch(userChatDataLoading());
-  try {
-    const { data } = await apiClient.get(`${apiURL}/api/v1/chats`, {
-      limit,
-      offset,
-    });
-    dispatch(userChatDataSuccess({ data }));
+export const getUsersChatData = ({ limit, offset }) => async (dispatch) => {
+	dispatch(userChatDataLoading());
+	try {
+		const { data } = await apiClient.get(`${apiURL}/api/v1/chats`, {
+			limit,
+			offset
+		});
+		dispatch(userChatDataSuccess({ data }));
 
-    return data;
-  } catch (error) {
-    dispatch(userChatDataFailure(error.message));
-  }
+		return data;
+	} catch (error) {
+		dispatch(userChatDataFailure(error.message));
+	}
 };
 
-export const getChatsWithParams = ({ search }) => async dispatch => {
-  dispatch(userChatDataLoading({ search }));
-  try {
-    console.log(search);
-    const data = await apiClient.get(
-      `${apiURL}/api/v1/chats?search=%${search}%`,
-    );
-    dispatch(userChatDataSuccess(data));
-    console.log(data);
-    //return data;
-  } catch (error) {
-    dispatch(userChatDataFailure(error.message));
-  }
+export const getChatsWithParams = ({ search }) => async (dispatch) => {
+	dispatch(userChatDataLoading({ search }));
+	try {
+		const data = await apiClient.get(`${apiURL}/api/v1/chats?search=%${search}%`);
+		dispatch(userChatDataSuccess(data));
+	} catch (error) {
+		dispatch(userChatDataFailure(error.message));
+	}
 };
 
-export const createChat = parameters => async dispatch => {
-  dispatch(userChatDataLoading());
-  try {
-    const { data } = await apiClient.post(`${apiURL}/api/v1/chats`, parameters);
-    dispatch(newChatDataSuccess(data));
-  } catch (error) {
-    dispatch(userChatDataFailure(error.message));
-  }
+export const createChat = (parameters) => async (dispatch) => {
+	dispatch(userChatDataLoading());
+	try {
+		const { data } = await apiClient.post(`${apiURL}/api/v1/chats`, parameters);
+		dispatch(newChatDataSuccess(data));
+	} catch (error) {
+		dispatch(userChatDataFailure(error.message));
+	}
 };
 
 // eslint-disable-next-line unicorn/consistent-function-scoping
-export const resetChatState = () => dispatch => {
-  dispatch(clearChatState());
+export const resetChatState = () => (dispatch) => {
+	dispatch(clearChatState());
 };
--- client/src/reducers/chats.js
+++ client/src/reducers/chats.js
@@ -1,40 +1,40 @@
 import {
-  USERS_CHAT_DATA_LOADING,
-  USERS_CHAT_DATA_LOADING_DATA_SUCCESS,
-  USERS_CHAT_DATA_LOADING_DATA_FAILURE,
-  USERS_CHAT_ADD_OWN_CHAT,
-  USERS_CHAT_RESET_STATE,
+	USERS_CHAT_DATA_LOADING,
+	USERS_CHAT_DATA_LOADING_DATA_SUCCESS,
+	USERS_CHAT_DATA_LOADING_DATA_FAILURE,
+	USERS_CHAT_ADD_OWN_CHAT,
+	USERS_CHAT_RESET_STATE
 } from '../constants';
 
 const initialState = {
-  data: [],
-  error: '',
-  isLoading: true,
-  userOption: { id: '' },
+	data: [],
+	error: '',
+	isLoading: true,
+	userOption: { id: '' }
 };
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case USERS_CHAT_DATA_LOADING_DATA_SUCCESS:
-      return {
-        ...state,
-        data: [...state.data, ...action.payload.data],
-        error: '',
-        isLoading: false,
-      };
-    case USERS_CHAT_DATA_LOADING:
-      return { ...state };
-    case USERS_CHAT_ADD_OWN_CHAT:
-      return {
-        ...state,
-        data: [...state.data, action.payload],
-        isLoading: false,
-      };
-    case USERS_CHAT_DATA_LOADING_DATA_FAILURE:
-      return { ...state, error: action.error, isLoading: false };
-    case USERS_CHAT_RESET_STATE:
-      return { ...state, data: [] };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case USERS_CHAT_DATA_LOADING_DATA_SUCCESS:
+			return {
+				...state,
+				...action.payload,
+				error: '',
+				isLoading: false
+			};
+		case USERS_CHAT_DATA_LOADING:
+			return { ...state };
+		case USERS_CHAT_ADD_OWN_CHAT:
+			return {
+				...state,
+				data: [ ...state.data, action.payload ],
+				isLoading: false
+			};
+		case USERS_CHAT_DATA_LOADING_DATA_FAILURE:
+			return { ...state, error: action.error, isLoading: false };
+		case USERS_CHAT_RESET_STATE:
+			return { ...state, data: [] };
+		default:
+			return state;
+	}
 };
