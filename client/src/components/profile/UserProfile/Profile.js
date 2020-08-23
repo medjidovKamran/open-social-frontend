d6a6184ce705719aead70e390d9c04ebbefadd48
Movchan94 <tkd.movchan@gmail.com>
2020-08-23 18:09:13 +0300

    added refresh page after upload avatar

--- client/src/components/profile/UserProfile/Profile.js
+++ client/src/components/profile/UserProfile/Profile.js
@@ -1,28 +1,26 @@
-import React, {Component} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCaretDown, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import withStyles from 'isomorphic-style-loader/withStyles';
-import {Card, Col, Container, Row} from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import styles from './Profile.scss';
 import stylesButton from './ProfileButton.scss';
-import {ProfileButton} from './ProfileButton/ProfileButton';
+import { ProfileButton } from './ProfileButton/ProfileButton';
 import 'react-tabs/style/react-tabs.css';
 import TabsComponent from './TabsComponent/TabsComponent';
 import ProfilePhoto from './ProfilePhoto/ProfilePhoto';
 import OwnChatButton from './OwnChat';
 import apiClient from '../../../utils/axios-with-auth';
-import {apiURL} from '../../../constants';
 import defaultUserPhoto from '../../../assets/default_user_profile.jpg';
-import Link from '../../Link';
 import textData from '../../../utils/lib/languages.json';
 
 class Profile extends Component {
   static propTypes = {
     avatar: PropTypes.shape({
       avatar: PropTypes.shape({
-        name: PropTypes.string,
+        url: PropTypes.string,
       }),
     }).isRequired,
     id: PropTypes.shape({
@@ -30,63 +28,30 @@ class Profile extends Component {
     }).isRequired,
   };
 
-  state = {
-    isPhotoLoaded: false,
-    photo: '',
-  };
-
   getUserAvatar() {
-    const {avatar} = this.props.avatar;
-    console.log(this.props)
+    const { avatar } = this.props.avatar;
     if (avatar == null) {
       return defaultUserPhoto;
-    } else {
-      return avatar.url;
     }
+    return avatar.url;
   }
 
-  componentDidMount() {
-    const {avatar} = this.props;
-    if (avatar) {
-      this.setState(previousState => ({
-        isDisplayed: !previousState.isDisplayed,
-        isPhotoLoaded: !previousState.isPhotoLoaded,
-      }));
-    }
+  refreshPage() {
+    window.location.reload(false);
   }
-
-  changeProfilePhotoHandler = () => {
-    this.setState(previousState => ({
-      isDisplayed: !previousState.isDisplayed,
-    }));
-  };
 
   loadPhoto = event => {
     const photo = event.target.files[0];
-
     apiClient.saveUserProfilePhoto(photo);
-
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(photo);
-    fileReader.addEventListener('load', () => {
-      const {result} = fileReader;
-      if (result) {
-        this.setState(previousState => ({
-          isDefaultPhotoDisplayed: false,
-          isDisplayed: !previousState.isDisplayed,
-          isPhotoLoaded: !previousState.isPhotoLoaded,
-          photo: result,
-        }));
-      }
-    });
+    this.refreshPage();
   };
 
   render() {
     const {
-      id: {id},
+      id: { id },
       lang,
     } = this.props;
-    const {profilePage} = textData;
+    const { profilePage } = textData;
     return (
       <Container className={styles.UserProfile}>
         <Card className={styles.ProfileCard}>
@@ -94,7 +59,6 @@ class Profile extends Component {
             <Col lg={5} md={5} sm={12}>
               <ProfilePhoto
                 imgSource={this.getUserAvatar()}
-                changeProfilePhotoHandler={this.changeProfilePhotoHandler}
                 loadPhoto={this.loadPhoto}
               />
               <div>
@@ -123,12 +87,12 @@ class Profile extends Component {
                   }
                 />
                 {id === apiClient.userId() && (
-                  <OwnChatButton nameBtn={profilePage.buttons.chat[lang]}/>
+                  <OwnChatButton nameBtn={profilePage.buttons.chat[lang]} />
                 )}
               </div>
             </Col>
             <Col lg={7} md={7} sm={12}>
-              <TabsComponent/>
+              <TabsComponent />
             </Col>
           </Row>
         </Card>
@@ -161,7 +125,7 @@ class Profile extends Component {
 Profile.whyDidYouRender = true;
 
 export default connect(
-  ({userProfile: avatar, userProfile: id, menu: {lang}}) => ({
+  ({ userProfile: avatar, userProfile: id, menu: { lang } }) => ({
     avatar,
     id,
     lang,
