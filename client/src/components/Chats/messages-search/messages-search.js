0f755fd797f10b80fdc670f278403516821e5d80
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-08-25 19:13:05 +0300

    message search

--- client/src/components/Chats/messages-search/messages-search.js
+++ client/src/components/Chats/messages-search/messages-search.js
@@ -8,51 +8,40 @@ import SearchIcon from '@material-ui/icons/Search';
 import s from './messages-search.scss';
 
 class MessageSearchPanel extends Component {
-  static propTypes = {
-    getMessagesWithParams: PropTypes.func.isRequired,
-  };
-  state = {
-    search: '',
-  };
+	static propTypes = {
+		getMessagesWithParams: PropTypes.func.isRequired
+	};
+	state = {
+		search: ''
+	};
 
-  handleOnInputChange = _.debounce(search => {
-    this.setState({ search: search });
-    this.props.getMessagesWithParams({ search });
-  }, 700);
+	handleOnInputChange = _.debounce((search) => {
+		this.setState({ search: search });
+		this.props.getMessagesWithParams({ search });
+	}, 700);
 
-  //   shouldComponentUpdate = (messages) => {
-  //     messages.map(message => (
-  //       <div key={message.id}>
-  //         <Message message={message} />
-  //       </div>
-  //     ));
-  //   };
-
-  render() {
-    return (
-      <div className={s.SearchMessages}>
-        <input
-          className={s.SearchInputMessages}
-          type="text"
-          value={this.state.query}
-          placeholder="Search in messages..."
-          onChange={e => this.handleOnInputChange(e.target.value)}
-        />
-        <SearchIcon />
-      </div>
-      //</div>
-      //      this.shouldComponentUpdate
-      //</div>
-    );
-  }
+	render() {
+		return (
+			<div className={s.SearchMessages}>
+				<input
+					className={s.SearchInputMessages}
+					type="text"
+					value={this.state.query}
+					placeholder="Search in messages..."
+					onChange={(e) => this.handleOnInputChange(e.target.value)}
+				/>
+				<SearchIcon />
+			</div>
+		);
+	}
 }
 
 MessageSearchPanel.whyDidYouRender = true;
 export default connect(
-  ({ userChats: { data, error, isLoading } }) => ({
-    data,
-    error,
-    isLoading,
-  }),
-  { getMessagesWithParams },
+	({ userChats: { data, error, isLoading } }) => ({
+		data,
+		error,
+		isLoading
+	}),
+	{ getMessagesWithParams }
 )(withStyles(s)(React.memo(MessageSearchPanel)));
